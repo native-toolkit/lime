b5cfdbb5f0937adad1bb14b43607a41232b4965d
Lily <egzozu.be.bas@gmail.com>
2024-10-26 15:27:49 +0300

    whoops

--- project/lib/custom/pixman/config.h
+++ project/lib/custom/pixman/config.h
@@ -310,7 +310,7 @@
 /* #undef USE_VMX */
 
 /* use x86 MMX compiler intrinsics */
-#if ((defined(HX_WINDOWS) && !defined(HXCPP_ARM64)) || /*defined(HX_MACOS) ||*/ (defined(HX_LINUX) && !defined(RASPBERRYPI)) ) && !defined(HXCPP_M64) && !defined(HXCPP_ARMV7) !defined(HXCPP_ARM64)
+#if ((defined(HX_WINDOWS) && !defined(HXCPP_ARM64)) || /*defined(HX_MACOS) ||*/ (defined(HX_LINUX) && !defined(RASPBERRYPI)) ) && !defined(HXCPP_M64) && !defined(HXCPP_ARMV7) && !defined(HXCPP_ARM64)
 #define USE_X86_MMX 1
 #else
 /* #undef USE_X86_MMX */
