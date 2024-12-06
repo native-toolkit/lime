fb194f02f55373da922f59e2f9e3cbbfd1cd2b7f
unknown <luigibros626@gmail.com>
2024-12-06 19:09:10 +0100

    Bump version in version.h

--- project/lib/custom/openal/build/version.h
+++ project/lib/custom/openal/build/version.h
@@ -1,9 +1,9 @@
 /* Define to the library version */
-#define ALSOFT_VERSION "1.23.1"
-#define ALSOFT_VERSION_NUM 1,23,1,0
+#define ALSOFT_VERSION "1.24.1"
+#define ALSOFT_VERSION_NUM 1,24,1,0
 
 /* Define the branch being built */
 #define ALSOFT_GIT_BRANCH "HEAD"
 
 /* Define the hash of the head commit */
-#define ALSOFT_GIT_COMMIT_HASH "c68b793"
+#define ALSOFT_GIT_COMMIT_HASH "90191ed"
