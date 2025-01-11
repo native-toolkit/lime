4270cfde9deeea01be462010702bb2da316ea36d
unknown <luigibros626@gmail.com>
2025-01-11 14:32:49 +0100

    Update OpenAL Soft to 1.24.2

--- project/lib/custom/openal/build/version.h
+++ project/lib/custom/openal/build/version.h
@@ -1,9 +1,9 @@
 /* Define to the library version */
-#define ALSOFT_VERSION "1.24.1"
-#define ALSOFT_VERSION_NUM 1,24,1,0
+#define ALSOFT_VERSION "1.24.2"
+#define ALSOFT_VERSION_NUM 1,24,2,0
 
 /* Define the branch being built */
 #define ALSOFT_GIT_BRANCH "HEAD"
 
 /* Define the hash of the head commit */
-#define ALSOFT_GIT_COMMIT_HASH "90191ed"
+#define ALSOFT_GIT_COMMIT_HASH "b621b5f"
